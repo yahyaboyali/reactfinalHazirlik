e0a5a4466dad15212be6a124d392ddac0435fc8f
yahya boyalı <yahyaboyali@gmail.com>
2022-06-07 20:26:28 +0300

    tutorials added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import React from 'react';
 import './App.css';
-
 function App() {
+  function Deneme(){
+    const shoot = ()=>{
+      alert("shoot")
+
+    }
+    return <button onClick={shoot}>Take the shot!</button>;
+  }
+  class Deneme2 extends React.Component {
+    render(){
+      return <p>deneme2</p>
+    }
+  }
+  function ForProps(props){
+    return <p>{props.color} deneme props</p>
+  }
+  function IfDeneme(props){
+    const isGoal = props.isGoal;
+    if(isGoal){
+      return <p>gol oldu lan mal</p>;
+    }else{
+      return <p>enayi kaçırdı ya</p>
+    }
+  }
+  function DahaGuzelIf(props){
+    const isGoal = props.isGoal;
+    return <>
+    {isGoal? <p>gol oldu lan daha güzle</p>: <p>gol kaçtı lan mal daha güzel</p>}
+    </>
+  }
+  function Car(props){
+    return <li>{props.name}</li>
+  }
+  function Liste(){
+    const cars = [
+      {id: 1, name: 'Ford'},
+      {id: 2, name: 'BMW'},
+      {id: 3, name: 'Audi'}
+    ];
+    return(
+      <ul>
+        {cars.map((car)=><Car key={car.id} name={car.name}/>)}
+      </ul>
+    );
+  }
   return (
     <div className="App">
-      naber 1121
+      <Deneme/>
+      <Deneme2/>
+      <ForProps color="kırmızı"/>
+      <IfDeneme isGoal={false}/>
+      <DahaGuzelIf isGoal={false}/>
+      <Liste name={["de","dee"]}/>
     </div>
   );
 }
